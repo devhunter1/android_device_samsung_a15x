a537ee1b58a3c92d4bf2f3dc5094f2a445f9adf1
devhunter1 <anubhabkundu2@gmail.com>
2024-08-14 13:35:50 +0530

    added required blobs

--- device.mk
+++ device.mk
@@ -4,4 +4,25 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/samsung/a15x
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery
+
+# Health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+    
+# MTK plpath utils
+PRODUCT_PACKAGES += \
+    mtk_plpath_utils \
+    mtk_plpath_utils.recovery
+    
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
